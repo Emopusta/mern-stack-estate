2a9f7671a30d225cabac76a15bbc05672616416b
Emopusta <emreduman225@hotmail.com>
2023-10-31 17:06:46 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,44 @@
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Emopusta Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+        aliquam eleifend diam sed finibus. Pellentesque metus justo, ultrices in
+        finibus et, sodales scelerisque mi. Donec leo risus, congue ac luctus
+        ut, varius id magna. Quisque rhoncus ullamcorper sem eget laoreet.
+        Integer rhoncus felis sed tortor sodales, et sollicitudin nulla
+        fringilla. Integer id rutrum libero. Duis tempor molestie dapibus. Nam
+        fringilla eros a elit ullamcorper, eu interdum enim semper.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Maecenas euismod vitae leo non vulputate. Aliquam pellentesque massa nec
+        est congue, sed pulvinar erat vulputate. Aliquam enim lectus, commodo id
+        orci sit amet, varius lobortis tortor. Pellentesque nec mi scelerisque,
+        egestas augue sit amet, mollis massa. Sed vitae risus purus. Maecenas id
+        mi efficitur, tincidunt libero a, suscipit tellus. Integer cursus,
+        turpis ac molestie euismod, felis leo bibendum orci, eget bibendum felis
+        augue sed tortor. Nam eget mauris pharetra, vulputate leo ut, semper
+        ante. Integer lacus est, tristique sed ante aliquet, porta feugiat
+        libero. Cras dictum augue non accumsan luctus. Integer molestie felis
+        nunc, at commodo urna varius id. Cras id ante et diam vestibulum
+        tincidunt. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        In pellentesque diam eget porta dignissim.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Nullam pellentesque lorem sed dui pulvinar pulvinar. Duis non consequat
+        lectus. Suspendisse volutpat dictum mi, at facilisis velit commodo a.
+        Suspendisse ultrices, ex vitae semper sagittis, mi enim varius lorem, a
+        scelerisque elit nisl vitae justo. Integer sed urna laoreet, tristique
+        ligula ut, venenatis lacus. Duis condimentum dui sit amet molestie
+        dapibus. In semper aliquam neque, eu porta augue maximus non. Quisque
+        tincidunt, tortor at venenatis volutpat, est leo pulvinar urna, a
+        gravida nulla dolor congue tellus. Nam odio ante, hendrerit sit amet
+        faucibus eu, fringilla sit amet enim. Vestibulum aliquet condimentum
+        odio, vitae lacinia lacus pharetra non. Curabitur consectetur in diam
+        non consectetur. Quisque vulputate eu tellus eu fermentum. Curabitur
+        gravida est vel nisi tristique, vel ultricies nisl lacinia.
+      </p>
+    </div>
+  );
 }
