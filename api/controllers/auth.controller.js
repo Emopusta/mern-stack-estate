8cc1a007feff84280cb4d3fc97212d7e085070af
Emopusta <emreduman225@hotmail.com>
2023-10-31 20:08:22 +0300

    bugFix password empty

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,6 +20,7 @@ export const signin = async (req, res, next) => {
     const {email, password} = req.body;
     const validUser = await User.findOne({email: email});
     if (!validUser) return next(errorHandler(404, "User not found!"))
+    if(!password ==="") return next(errorHandler(401, 'Password can not empty!'));
     const validPassword = await bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
     const token = jwt.sign({ id:validUser._id}, process.env.JWT_SECRET)
