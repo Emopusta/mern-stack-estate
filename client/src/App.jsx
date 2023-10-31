7aa53d7c9200ea21def23d3f96d549dfaf4ffa53
Emopusta <emreduman225@hotmail.com>
2023-10-31 11:52:44 +0300

    create search page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
+import Search from "./pages/Search";
 
 export default function App() {
   return <BrowserRouter>
@@ -19,6 +20,8 @@ export default function App() {
     <Route path="/sign-up" element={<SignUp/>}></Route>
     <Route path="/about" element={<About/>}></Route>
     <Route path="/listing/:listingId" element={<Listing/>}></Route>
+    <Route path="/search" element={<Search/>}></Route>
+
 
     <Route element={<PrivateRoute/>}>
     <Route path="/create-listing" element={<CreateListing/>}></Route>
